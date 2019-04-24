37cee36c4e49634e4f6f5e424b338f87bd7a4540
Sarah Ahmad <sarahahmad2019@gmail.com>
2019-04-24 16:43:07 -0500

    pushing initial space changes

---
+++ Assets/Space.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space : MonoBehaviour
+{
+    public int MAX_STARTING_PLOTS = 9;
+    public int MAX_STARTING_STREETS = 8;
+    public GameObject[] PlotsList;
+    public GameObject[] StreetsList;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
