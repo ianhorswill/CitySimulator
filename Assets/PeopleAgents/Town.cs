53e13aa7dfb50fa0de00b954b6c37c5a1059b607
RY <32229224+Pikademic@users.noreply.github.com>
2019-04-28 13:36:36 -0500

    Updated Step function

--- Assets/PeopleAgents/Town.cs
+++ Assets/PeopleAgents/Town.cs
@@ -31,6 +31,9 @@ public class PersonTown //: Settlement
     public var aliveResidents = new List<Person>();
     public var deceased = new List<Person>();
 
+    private int deathProbaility = 10;
+    private int birthProbability = 20;
+
     //For David: Going to add another constructor to People that allows the creation of adults
     //     public Person (string name, List<Person> currSiblings, int age, Person sigOther, List<Person> children, Person[] parents, bool biologicalSex){
 
@@ -52,22 +55,8 @@ public class PersonTown //: Settlement
     }
 
     //Is this a life event?  Also may separate the system randomness / choosing from the exact method, instead using a parameter of a Person and just doing the effects on the Lists.
-    public static void death(){
-        /*Linq Method
-        //Must Query all living inhabitants and select one to become deceased...?
-        var livingPeople = from People in people
-                           where People.alive == true
-                           select Agent;
-        */
+    public static void death(Person selectedToDie){
         
-        //OOP Method:
-        //Now that have all living Agents, randomly select one to die, very un-nuanced for now
-        Random rng = new Random();
-        Person selectedToDie = aliveResidents.ElementAt(rng.Next(0, aliveResidents.Count()));
-
-        //Set flag of selectedAgent to non-living or deceased?
-        //Is Agent a GameObject?
-        selectedToDie.alive = false;
 
         //Shotty version at removing a person from associated lists
         //Person search = settlers.Find(x => x.id == selectedToDie.id);
@@ -77,37 +66,72 @@ public class PersonTown //: Settlement
         // Life event does further processing...?
     }
 
-    //Is this a life event?
-    public static void birth(){
-        //Select Parent Randomly: LINQ
-        /*
-        var livingPeople =  from Agent in Agents
-                            where Agent.getLivingStatus() == true
-                            select Agent;
-        */
-        //OOP Method:
-        bool found = false;
-
-        //Maximum Attempts a birth event will be attempted, should lend itself to being more sparce/random.  The value of 5 is arbitrary.
-        //Approach can be varied depending on whether a baby MUST be found, in that case would do randoms until success.
-        int maxAttempts = 5;
-        Person baby;
-
-        while(found != true && maxAttempts>0){
-            Random r = new Random();
-            Person selectedParent = livingPeople.ElementAt(r.Next(0, livingPeople.Count()));
-            Person newborn = createChild(selectedParent, selectedParent.sigOther);
-            if(newborn != null){
-                found = true;
-                baby = newborn;
-            }
-            maxAttempts--;                
-        }
-        if(!found){
-            Console.write("Birth Failed, parents not found or exceeded maximum attempts");
-            return;
-        }
+    public static void birth(Person baby){
         aliveResidents.Add(baby);
+    }
+
+    //Every timestep this method is called.
+    public static void step(){
+        Random rng = new Random();        
+        /* Birth, right now can only occur once per step */
+
+            //Select Parent Randomly: LINQ
+            /*
+            var livingPeople =  from Agent in Agents
+                                where Agent.getLivingStatus() == true
+                                select Agent;
+            */
+        //OOP Method:
+        int birthDice = rng.Next(100);
+        if(birthDice < birthProbaility){
+
+            bool found = false;
+
+            //Maximum Attempts a birth event will be attempted, should lend itself to being more sparce/random in accordance to the population.  The value of 5 is arbitrary.
+            //Approach can be varied depending on whether a baby MUST be found, in that case would do randoms until success.
+            int maxAttempts = 5;
+            Person baby;
+
+            while(found != true && maxAttempts>0){
+                Random r = new Random();
+                Person selectedParent = livingPeople.ElementAt(r.Next(0, livingPeople.Count()));
+                Person newborn = createChild(selectedParent, selectedParent.sigOther);
+                if(newborn != null){
+                    found = true;
+                    baby = newborn;
+                }
+                maxAttempts--;                
+            }
+            if(!found){
+                Console.write("Birth Failed, parents not found or exceeded maximum attempts");
+            }
+            else{
+                birth(baby);
+            }            
+        }
+
+    /* Death,  right now can only occur once per step */
+        /*Linq Method
+        //Must Query all living inhabitants and select one to become deceased...?
+        var livingPeople = from People in people
+                           where People.alive == true
+                           select Agent;
+        */
+        
+        //OOP Method:
+        //Now that have all living Agents, randomly select one to die, at a 10% chance
+
+        int deathDice = rng.Next(100);
+        if(deathDice < deathProbaility){
+            //Someone is dying
+            Person selectedToDie = aliveResidents.ElementAt(rng.Next(0, aliveResidents.Count()));
+
+            //Set flag of selectedAgent to non-living or deceased?
+            //Is Agent a GameObject?
+            selectedToDie.alive = false;
+
+            death(selectedToDie);
+        }
     }
 }
 
