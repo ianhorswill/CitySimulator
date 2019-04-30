a434e7a10858123edfe494c9401dece1638dafac
Yunwen Wang <yunwen.wang.b@gmail.com>
2019-04-30 10:57:49 -0500

    Construct Institution through Construction Company

--- Assets/Institution/Institution.cs
+++ Assets/Institution/Institution.cs
@@ -7,23 +7,30 @@ using System.Text;
 namespace Institution
 {
     // the InstitutionGenerator includes methods needed to generate an Institution
-    public static class InstitutionGenerator
+    public static class InstitutionManager
     {
         // store all the institutions been constructed
         private static List<Institution> institutionList;
         // store the hardcode institution types
         private static string[] institutionTypeList;
+        // store the construction companies
+        private static List<ConstructionCompany> constructionCompanyList;
         
-        static InstitutionGenerator()
+        static InstitutionManager()
         {
             institutionList = new List<Institution>();
-            string path = Directory.GetCurrentDirectory();
-            institutionTypeList = File.ReadAllLines(path +"/institutionTypes.txt");
+            constructionCompanyList =  new List<ConstructionCompany>();
+            institutionTypeList = File.ReadAllLines(Directory.GetCurrentDirectory() +"/institutionTypes.txt");
+            
+            // hard-codly assign one initial construction company
+            ConstructionCompany cons = new ConstructionCompany("Government", "0, 0", "ConstructionCompany", false);
+            constructionCompanyList.Add(cons);
+            institutionList.Add(cons);
         }
         
         // get a random type
         // TODO: based on current institution numbers and types and city scale
-        private static string getRandomType()
+        public static string GetRandomType()
         {
             Random rnd = new Random();
             int index = rnd.Next(0, institutionTypeList.Length);
@@ -32,9 +39,17 @@ namespace Institution
         
         
         // generate a new institution
-        public static Institution generatorInstitution(string owner, string location)
+        public static Institution GeneratorInstitution(string owner, string location)
         {
-            string type = getRandomType();
+            // the first institution have to be the ConstructionCompany
+//            if (institutionList.Count == 0)
+//            {
+//                ConstructionCompany cons = new ConstructionCompany(owner, location, "ConstructionCompany");
+//                constructionCompanyList.Add(cons);
+//                institutionList.Add(cons);
+//            }
+            
+            string type = GetRandomType();
             Institution newInstitution;
             switch (type)
             {
@@ -57,10 +72,21 @@ namespace Institution
                     newInstitution = new Institution(owner, location, type);
                     break;
             }
-            
+
+            if (newInstitution.getType().Equals("ConstructionCompany"))
+            {
+                constructionCompanyList.Add(newInstitution as ConstructionCompany);
+            }
             institutionList.Add(newInstitution);
             
             return newInstitution;
+        }
+
+        public static ConstructionCompany GetRandomConstructionCompany()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, constructionCompanyList.Count);
+            return constructionCompanyList[index];
         }
     }
 
@@ -71,12 +97,29 @@ namespace Institution
         private List<String> employeeList;
         private string type;
 
-        public Institution(string owner, string location,string type)
+        public Institution(string owner, string location,string type, bool needBuild = true)
         {
+            if (needBuild)
+            {
+                ConstructCompanySite(location);
+            }
+            
             this.owner = owner;
             this.location = location;
             this.type = type;
             employeeList = new List<string>();
+            
+            Console.WriteLine("------------------\n"+ this.ToString());
+        }
+        public Institution(string owner, string location,string type)
+        {
+            ConstructCompanySite(location);
+            
+            this.owner = owner;
+            this.location = location;
+            this.type = type;
+            employeeList = new List<string>();
+            Console.WriteLine("------------------\n"+ this.ToString());
         }
 
         public void Hiring(string person)
@@ -87,24 +130,35 @@ namespace Institution
 
         public void ConstructCompanySite(string location)
         {
-            
+            // randomly choose a construct company to construct a company site
+            ConstructionCompany cons = InstitutionManager.GetRandomConstructionCompany();
+            cons.Build(location);
         }
 
         public string getType()
         {
             return type;
         }
+
+        public override string ToString()
+        {
+            return "Institution: " + type + "\nOwner: " + owner + "\nLocation: (" + location + ")\n";
+        }
     }
 
     public class ConstructionCompany : Institution
     {
+        public ConstructionCompany(string owner, string location, string type, bool needBuild = true) : base(owner, location, type, needBuild)
+        {
+        }
+        
         public ConstructionCompany(string owner, string location, string type) : base(owner, location, type)
         {
         }
 
         public void Build(string loc)
         {
-            
+            Console.WriteLine("Construct building at "+loc);
         }
     }
 
@@ -173,14 +227,24 @@ namespace Institution
             return true;
         }
     }
+    
+    public static class Helper {
+        
+    }
 
-//    public class MainClass
-//    {
-//        public static void Main(string[] args)
-//        {
-//            Institution ins = InstitutionGenerator.generatorInstitution("", "");
-//            Console.WriteLine(ins.getType());
-//        }
-//    }
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var name1 = "John";
+            var location1 = "12, 7";
+
+            var name2 = "Alice";
+            var location2 = "3, 3";
+            
+            Institution ins = InstitutionManager.GeneratorInstitution(name1, location1);
+            Institution ins2 = InstitutionManager.GeneratorInstitution(name2, location2);
+        }
+    }
     
 }
