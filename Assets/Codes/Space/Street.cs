e277ea575288a40c5aa39881f98bcce4ecb59dbf
Junhao Li <junhaoli2019@u.northwestern.edu>
2019-05-14 20:34:30 -0500

    added plot coloring and made grid length 10

--- Assets/Codes/Space/Street.cs
+++ Assets/Codes/Space/Street.cs
@@ -7,8 +7,7 @@ public class Street : Object
     public enum street_direction { NS, EW }
     public string streetName;
     public int direction; // [0-1 -> NS EW]
-    static int CONNECTED_STREETS_MAX = 10; // arbitrary max of 10
-    public Street[] connected_streets = new Street[CONNECTED_STREETS_MAX];
+    public List<Street> connected_streets = new List<Street>();
 
     public Street(int street_dir, string name)
     {
