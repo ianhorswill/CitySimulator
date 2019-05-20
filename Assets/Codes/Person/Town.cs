1f95b02291236bd6d646133b2273a91e35a24f0e
Pikademic <pikapen@gmail.com>
2019-05-20 16:48:05 -0500

    Updated Education Town.cs step
    No longer gets a random construction company and utilizes the Institution dictionary structure

--- Assets/Codes/Person/Town.cs
+++ Assets/Codes/Person/Town.cs
@@ -208,7 +208,11 @@ public class PersonTown : SimulatorComponent
                             where (child != null && child.age <= 18 && child.workStatus.workplace == null)
                             select child;
         //Right now since construction companies are the most fleshed out, choose to send students to one; later when framework supports schools change this.
-        ConstructionCompany randomSchoolIfAny = InstitutionManager.GetRandomConstructionCompany();
+        List<Institution> schoolList = InstitutionManager.GetInstitutionOfType("School");
+        Institution randomSchoolIfAny = null;
+        if(schoolList.Count > 0){
+            randomSchoolIfAny = schoolList.RandomElement();
+        }
 
         if (under18NotInSchool != null && randomSchoolIfAny != null)
         {
@@ -221,7 +225,7 @@ public class PersonTown : SimulatorComponent
 
         //If someone turns 19 and they were in school, they "graduate" and lose the school occupation status and their education field is updated.
         var is19InSchool = from newAdult in aliveResidents
-                            where (newAdult != null && newAdult.age == 19 && newAdult.workStatus.workplace.getType().Equals("ConstructionCompany"))
+                            where (newAdult != null && newAdult.age == 19 && newAdult.workStatus.workplace.getType().Equals("School"))
                             select newAdult;
         foreach (Person pa in is19InSchool)
         {
