c95fa9c51f6860a419a388a0a46153f018a349e6
DESKTOP-A5IC082\Samuel Hill <whatinthesamhill@protonmail.com>
2019-05-20 16:02:51 -0500

    Fixing action death issues

--- Assets/Codes/Action/Actions/ActionDeath.cs
+++ Assets/Codes/Action/Actions/ActionDeath.cs
@@ -1,26 +1,10 @@
 
+using System.Collections.Generic;
+
 public class ActionDeath : ActionType
 {
-    public int priority = 3;
-
     public override string ActionName => "Death";
     public override double Chance => 1.0;
 
-    public override bool prerequisites(object agent, object patient, Location location, Time time)
-    {
-        // TODO: check the prereqs of this specific action 
-        return ActionStatics.randomNumberGenerator.Next(100) < ActionStatics.DEATH_PROBABILITY;
-    }
-    
-    public override void modifications(object agent, object patient, Location location, Time time)
-    {
-        // TODO: modify the world
-        ActionStatics.aliveResidents.Remove((Person) agent);
-        ActionStatics.deceased.Add((Person) agent);
-    }
-    
-    public override void triggers(object agent, object patient, Location location, Time time)
-    {
-        // TODO: call all the actions that will be triggered by this action
-    }
+    public override List<RoleTypeBase> Role_list => throw new System.NotImplementedException();
 }
